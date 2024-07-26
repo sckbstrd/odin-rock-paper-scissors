b64b2563999820ebb3ce361facf667e9ef4fe1a3
sckbstrd <jguitarok@gmail.com>
2024-07-26 16:35:17 +0300

    Create playRound function, add game logic.

--- script.js
+++ script.js
@@ -21,6 +21,44 @@ function getComputerChoice() {
 }	
 
 function getHumanChoice() {
-	let choice = prompt("YOUR TURN: ").toLowerCase();
+	let choice = prompt("Rock? Paper? Scissors?: ").toLowerCase();
 	return choice;
 }
+
+function playRound(humanChoice, computerChoice) {
+	console.log("You: " + humanChoice + "\nComputer: "  + computerChoice)
+	if (humanChoice == computerChoice) {
+		return "It's a tie!";
+	}
+
+	if (humanChoice == "rock") {
+		if (computerChoice == "scissors") {
+			return "You won! Rock beats scissors."
+		} else {
+			return "You lost! Paper beats rock."
+		}
+	} 
+
+	if (humanChoice == "paper") {
+		if (computerChoice == "rock") {
+			return "You won! paper beats rock."
+		} else {
+			return "You lost! Scissors beats paper."
+		}
+	} 
+
+	if (humanChoice == "scissors") {
+		if (computerChoice == "paper") {
+			return "You won! Scissors beats paper."
+		} else {
+			return "You lost! Rock beats scissors."
+		}
+	} 
+
+	
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
